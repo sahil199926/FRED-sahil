a1448749eab9894b39eda5e902608d473b663e0f
Gyan Prakash Singh <gyan@eonlint.com>
2023-03-14 17:15:52 +0530

    firstpage

--- src/components/FirstPage/FirstPage.js
+++ src/components/FirstPage/FirstPage.js
@@ -1,83 +1,82 @@
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import React from "react";
 import { Bar } from "react-chartjs-2";
 import {
   BUDGET_CHART_DATA,
   BUDGET_CHART_OPTION,
 } from "../../assets/data/project";
+import BarCard from "../common/BarCard";
+import BarChart from "../common/BarChart";
 import Filters from "../common/Filters";
+import GraphCard from "../common/GraphCard";
 import PriseShowCard from "../common/PriseShowCard";
+
+import './index.css'
+
 export default function FirstPage() {
   return (
     <div>
       <Filters />
 
       {/* Edit by gyan */}
-      <Grid container spacing={2}>
-        <Grid item xs={2} lg={2}>
+      <div className="upperbar" style={{}}>
+        <Box sx={{width:{xs:"100%",lg:"16.68%"}}}>
           Safety
-        </Grid>
-        <Grid item xs={4}>
+        </Box>
+        <Box sx={{width:{xs:"100%",lg:"58.34%"}}}>
           Budget
-        </Grid>
-        <Grid item xs={6} lg={2}>
+        </Box>
+        <Box sx={{width:{xs:"100%",lg:"16%"}}}>
           Progress
-        </Grid>
-      </Grid>
+        </Box>
+      </div>
       <Grid container spacing={1}>
-        <Grid container item xs={12} lg={2}>
-          <Grid item xs={4} lg={12}>
-            <PriseShowCard
-            heading="Fatalities"
-            value="0"
-            currency="$"
-            subHeading="One TCO 2022 Target"
-
-            />
-          </Grid>
-          <Grid item xs={4} lg={12}>
-            <h3>Fatalities</h3>
-            <div>0</div>
-            <div>One TCO 2022 Target</div>
-          </Grid>
-          <Grid item xs={4} lg={12}>
-            <h3>Fatalities</h3>
-            <div>0</div>
-            <div>One TCO 2022 Target</div>
-          </Grid>
+        <Grid container item xs={12} lg={2} spacing={1}>
+          {[0, 1, 2].map((data) => {
+            return <Grid item xs={4} lg={12} key={data}>
+              <PriseShowCard
+                heading="Fatalities"
+                value="0"
+                currency="$"
+                subHeading="One TCO 2022 Target"
+              />
+            </Grid>;
+          })}
         </Grid>
-        <Grid container item xs={12} lg={6}>
-          <Grid item xs={6}>
-            <h3>Fatalities</h3>
-            <div>0</div>
-            <div>One TCO 2022 Target</div>
-          </Grid>
-          <Grid item xs={6}>
-            <h3>Fatalities</h3>
-            <div>0</div>
-            <div>One TCO 2022 Target</div>
-          </Grid>
-          <Grid item xs={12}>
+        <Grid container item xs={12} lg={5}>
+          {
+            [0,1].map((data)=>{
+              return <Grid item xs={6}>
+                <BarCard 
+                heading="budget"
+                currency="$"
+                value="200M"
+                />
+              </Grid>
+            })
+          }
+          <Grid item xs={12} lg={12}>
             {/* <div className="">Annual Outlook vs Budget</div> */}
-            <Bar options={BUDGET_CHART_OPTION} data={BUDGET_CHART_DATA} />
+            <BarChart />
+            {/* <Bar options={BUDGET_CHART_OPTION} data={BUDGET_CHART_DATA} /> */}
           </Grid>
         </Grid>
-        <Grid container item xs={12} lg={2}>
-          <Grid item xs={4} lg={12}>
-            <h3>Fatalities</h3>
-            <div>0</div>
-            <div>One TCO 2022 Target</div>
-          </Grid>
-          <Grid item xs={4} lg={12}>
-            <h3>Fatalities</h3>
-            <div>0</div>
-            <div>One TCO 2022 Target</div>
-          </Grid>
-          <Grid item xs={4} lg={12}>
-            <h3>Fatalities</h3>
-            <div>0</div>
-            <div>One TCO 2022 Target</div>
-          </Grid>
+        <Grid container item xs={12} lg={2} spacing={1}>
+          {
+            [0,1,2].map((data)=>{
+              return<Grid item xs={4} lg={12} key={data}>
+              <PriseShowCard
+                heading="Hours"
+                value="10K"
+                currency=""
+                subHeading=""
+              />
+            </Grid>;
+            })
+          }
+        </Grid>
+        <Grid item xs={12} lg={3} >
+          <GraphCard />
         </Grid>
       </Grid>
     </div>
---
+++ src/components/common/BarCard/index.js
@@ -0,0 +1,28 @@
+import { display } from '@mui/system';
+import React from 'react'
+
+const BarCard = (props) => {
+    const {
+        heading,
+        currency,
+        value,
+      }=props;
+      console.log(props)
+      return (
+        <div style={{
+          backgroundColor:"#fff", 
+          width:"254px",
+          height:"86px",
+          textAlign:"center",
+          borderRadius:"8px",
+          display:"flex",
+          flexDirection:"column",
+          justifyContent:"center"
+          }}>
+          <div style={{fontSize:"14px", padding:"2px 0px"}} ><b>{heading}</b></div>
+          <div style={{fontSize:"24px", padding:"0px 0px", color:"#3B873E"}} >{currency}{value}</div>
+        </div>
+      );
+}
+
+export default BarCard
