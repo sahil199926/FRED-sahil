e5ff962414d5925e330178eeff460048063b16fd
sahil jaryal <sahil199926@gmail.com>
2023-03-20 15:58:39 +0530

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,7 @@ export default function Footer() {
       style={{
         backgroundColor: "#0065b1",
         height: "45px",
-        position: "absolute",
+        // position: "absolute",
         // position: "sticky",
         // position: location.pathname === "/home" ? "" : "absolute",
         bottom: "0px",
